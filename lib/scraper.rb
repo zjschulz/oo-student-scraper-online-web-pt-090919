0b3d2c54475802c8e5d0b47f61b18ac269ecdf0f
Zachary Schulz <zjschulz@gmail.com>
2019-11-03 18:02:49 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,15 +1,35 @@
+require 'nokogiri'
 require 'open-uri'
 require 'pry'
 
 class Scraper
 
   def self.scrape_index_page(index_url)
-    
+    html = open("https://learn-co-curriculum.github.io/student-scraper-test-page/index.html")
+    doc = Nokogiri::HTML(html)
+    students = Array.new
+    hash = doc.css('div.student-card')
+    hash.each do |student_card|
+      student = {
+        name: student_card.css('h4.student-name').text,
+        location: student_card.css('p.student-location').text,
+        profile_url: student_card.css('a')[0].attributes['href'].value}
+      students << student
+    end
+    students
   end
+
+#"https://learn-co-curriculum.github.io/student-scraper-test-page/index.html" +
 
   def self.scrape_profile_page(profile_url)
-    
+    html = open(profile_url)
+    doc = Nokogiri::HTML(html)
   end
-
+#twitter
+#linkedin
+#github
+#blog 
+#profile_quote
+#bio
 end
 
--- lib/student.rb
+++ lib/student.rb
@@ -5,19 +5,21 @@ class Student
   @@all = []
 
   def initialize(student_hash)
-    
+    #should take in an argument of a hash and use meta-programming to assign the newly created student attributes and values per the key/value pairs of the hash.
+    @@all << self
   end
 
   def self.create_from_collection(students_array)
-    
+    #should take in an array of hashes. In fact, we will call Student.create_from_collection with the return value of the Scraper.scrape_index_page method as the argument
+    #should iterate over the array of hashes and create a new individual student using each hash
   end
 
   def add_student_attributes(attributes_hash)
-    
+    #should iterate over the given hash and use meta-programming to dynamically assign the student attributes and values per the key/value pairs of the hash.
   end
 
   def self.all
-    
+    @@all
   end
 end
 
